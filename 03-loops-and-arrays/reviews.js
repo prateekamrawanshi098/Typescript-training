9d86c55d72be42190f977e129e3f2dac1b88f8f7
Prateek Amrawanshi <prateekamrawanshimk7193@gmail.com@gmail.com>
2026-05-03 13:30:25 +0530

    arrays and loops on typescript

---
+++ 03-loops-and-arrays/reviews.js
@@ -0,0 +1,9 @@
+"use strict";
+let reviews = [3, 4.5, 3, 5, 2];
+let total = 0;
+for (let i = 0; i < reviews.length; i++) {
+    console.log(reviews[i]);
+    total = total + reviews[i];
+}
+let average = total / reviews.length;
+console.log(`total = ${total} and average= ${average}`);
